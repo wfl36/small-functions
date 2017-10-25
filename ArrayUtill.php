daf26b24d8c042e843b72d90ced2cf9879e2f72f
wangfenglei <wangfenglei@100tal.com>
2017-10-25 19:52:59 +0800

    二维数组排序

--- ArrayUtill.php
+++ ArrayUtill.php
@@ -36,4 +36,28 @@ class ArrayUtill
         }
         return $sign;
     }
+
+    /**
+     * 二维数组排序
+     * @param $multi_array
+     * @param $sort_key
+     * @param int $sort
+     * @return bool
+     */
+    public static function multi_array_sort($multi_array,$sort_key,$sort = SORT_ASC){
+        if(is_array($multi_array)){
+            foreach ($multi_array as $row_array){
+                if(is_array($row_array)){
+                    $key_array[] = $row_array[$sort_key];
+                }else{
+                    return false;
+                }
+            }
+        }else{
+            return false;
+        }
+
+        array_multisort($key_array,$sort,$multi_array);
+        return $multi_array;
+    }
 }
